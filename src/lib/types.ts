1071d29b7c791ee8bad39289717bbeffd70da381
drwhdgs <101528493+drwhdgs@users.noreply.github.com>
2025-09-26 11:22:16 -0600

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -151,7 +151,7 @@ export const PACKAGE_TEMPLATES: PackageTemplate[] = [
         name: "Manhattan Life Out-of-Pocket Protection",
         provider: "Manhattan Life",
         monthlyPremium: 0,
-        coverage: "$100/day Inpatient Hospital Confinement Benefit",
+        coverage: "$100/day Inpatient Hospital Confinement Benefit, $6,350 Hospital Admission Benefit",
         details: "An Out-of-Pocket policy can help the costs associated with a hospital stay from becoming an unnecessary burden.",
         brochureUrl: "https://apps.topbrokercrm.com/storage/files/aHB0RFsyM9OfQQ6aOgPWSBLP6tFBIs4miR11QMzE.pdf"
       }),
@@ -203,7 +203,7 @@ export const PACKAGE_TEMPLATES: PackageTemplate[] = [
         name: "Manhattan Life Out-of-Pocket Protection",
         provider: "Manhattan Life",
         monthlyPremium: 0,
-        coverage: "$200/day Inpatient Hospital Confinement Benefit",
+        coverage: "$200/day Inpatient Hospital Confinement Benefit, $5,000 Hospital Admission Benefit",
         details: "An Out-of-Pocket policy can help the costs associated with a hospital stay from becoming an unnecessary burden.",
         brochureUrl: "https://apps.topbrokercrm.com/storage/files/aHB0RFsyM9OfQQ6aOgPWSBLP6tFBIs4miR11QMzE.pdf"
       }),
@@ -282,7 +282,7 @@ export const PACKAGE_TEMPLATES: PackageTemplate[] = [
         name: "Manhattan Life Out-of-Pocket Protection",
         provider: "Manhattan Life",
         monthlyPremium: 0,
-        coverage: "$200/day Inpatient Hospital Confinement Benefit",
+        coverage: "$200/day Inpatient Hospital Confinement Benefit, $2,500 Hospital Admission Benefit",
         details: "An Out-of-Pocket policy can help the costs associated with a hospital stay from becoming an unnecessary burden.",
         brochureUrl: "https://apps.topbrokercrm.com/storage/files/aHB0RFsyM9OfQQ6aOgPWSBLP6tFBIs4miR11QMzE.pdf"
       }),
